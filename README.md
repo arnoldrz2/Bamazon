69d80695199049984b4b185daaf20f87006dd427
Arnold R Zimmerman Jr <arnoldrz2@gmail.com>
2018-02-01 17:59:54 -0600

    added npm package information/syntax to readme/bamazonCustomer js file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # bamazon
+
+NPM Packages Used:
+- Inquirer: https://www.npmjs.com/package/inquirer
+- MySQL: https://www.npmjs.com/package/mysql
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,25 @@
 var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+//MySQL Connection Setup Example
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
+
+
+//Inquirer Example
+inquirer.prompt([/* Pass your questions in here */]).then(answers => {
+    // Use user feedback for... whatever!!
+});
