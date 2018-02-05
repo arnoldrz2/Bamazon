9b65b629b35cff97f44a28fde414e127a63ce186
arnold <arnoldrz2@gmail.com>
2018-02-04 21:34:45 -0600

    added SQL DB data and created majority of first part of app

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,25 +1,64 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
 
+// var app = express();
+
+// var port = 4000;
+
 //MySQL Connection Setup Example
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'me',
-  password : 'secret',
-  database : 'my_db'
+  user     : 'root',
+  password : 'root',
+  database : 'bamazondb',
+  port     : 3306
 });
 
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
+connection.connect(function(err){
+  if(err) throw err;
+  console.log("Connected!");
+  displayProductInfo();
 });
 
-connection.end();
+function displayProductInfo() {
+  console.log("Welcome to Bamazon. Here are our products!\n");
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log(res);
+  });
+}
+
+function itemSelect() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    //Prompt User: "What would you like to buy?"
+    inquirer
+      .prompt([
+        {
+        name: "selectItemId",
+        type: "input",
+        choices: function() {
+          var choiceArray = [];
+          for (var i = 0, i < res.length; i++) {
+            choiceArray.push(results[i].product_name);
+          }
+          return choiceArray;
+        },
+        message: "Please enter the item_id number of the product you would like to purchase."
+      },
+      {
+        name: "unitPurchaseQuantity",
+        type: "input",
+        message: "How many would you like to purchase?"
+      }
+    ])
+    .then(function(answer){
+      if(answer)
+    })
+  }
 
 
-//Inquirer Example
-inquirer.prompt([/* Pass your questions in here */]).then(answers => {
-    // Use user feedback for... whatever!!
-});
+  //Inquirer Example
+  inquirer.prompt([/* Pass your questions in here */]).then(answers => {
+      // Use user feedback for... whatever!!
+  });
